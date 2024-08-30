9c21ed91edac70a5d684b243cfe42c541967891e
Luciferian Ink <LuciferianInk@protonmail.com>
2024-08-30 03:22:11 -0500

    fixed all bugs

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(name='thorns',
       packages=find_packages(), 
       install_requires=[
+            'hivemind @ git+https://github.com/learning-at-home/hivemind.git@213bff98a62accb91f254e2afdccbf1d69ebdea9',
             'numpy',
             'torch',
             'transformers'
