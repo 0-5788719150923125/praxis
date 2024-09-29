9f0e072dbd3585745a72c68119a3ab0229dea185
Luciferian Ink <LuciferianInk@protonmail.com>
2024-09-29 09:55:09 -0500

    save

--- praxis/modules/router.py
+++ praxis/modules/router.py
@@ -40,62 +40,56 @@ class PraxisMixtureOfDepths(nn.Linear):
         if self.training:
             #  𝑟𝑙> 𝑃𝛽 (R) - equation 1
             token_weights, token_indices = torch.topk(
-                # page 7: the [aux] loss centers the sigmoid of the router’s outputs around 0.5
                 router_logits,
                 k,
                 dim=1,
                 sorted=False,
             )
         else:
-            # top-k breaks causality; the sigmoid here allows us to sample autoregressively during inference
+            # top-k breaks causality; a sigmoid operation allows us to sample
+            # autoregressively regardless, during inference
             token_mask = torch.sigmoid(router_logits) > 0.5
             token_indices = torch.nonzero(token_mask, as_tuple=True)[1].view(b, -1)
 
             if token_indices.numel() > 0:
                 token_weights = (
-                    router_logits.squeeze(-1).gather(b, token_indices).unsqueeze(-1)
+                    router_logits.squeeze(-1).gather(1, token_indices).unsqueeze(-1)
                 )
             else:
                 # if no tokens were selected, just use the most recent k tokens
-                select_tokens = min(k, s)
+                selected_tokens = min(k, s)
                 token_indices = (
-                    torch.arange(s - select_tokens, s, device=inputs.device)
+                    torch.arange(s - selected_tokens, s, device=inputs.device)
                     .view(1, -1)
                     .expand(b, -1)
                 )
-                token_weights = torch.ones(b, select_tokens, 1, device=inputs.device)
+                token_weights = torch.ones(b, selected_tokens, 1, device=inputs.device)
 
             token_indices = token_indices.unsqueeze(-1)
 
-        # required to maintain the casual nature of an autoregressive model
-        sorted_index_values, sorted_index_indices = torch.sort(token_indices, dim=1)
-
         # select idx for copying for original tensor
-        indices_expanded = sorted_index_values.expand(-1, -1, d)
+        indices_expanded = token_indices.expand(-1, -1, d)
 
         # filtered topk tokens with a capacity of C
         filtered_inputs = torch.gather(
             input=inputs, dim=1, index=indices_expanded
         )  # -> batch, capacity, 1
 
-        # selecting router weight by idx
-        router_weights = torch.gather(token_weights, dim=1, index=sorted_index_indices)
-
         # slice the attention mask based on the selected token indices
         filtered_attention_mask = torch.gather(
             input=attention_mask,
             dim=1,
-            index=sorted_index_values.squeeze(-1),
+            index=token_indices.squeeze(-1),
         )
 
         # pass the selected tokens through the transformer block
         expert_outputs = expert(
             filtered_inputs,
             attention_mask=filtered_attention_mask,
-            router_weights=router_weights,
+            router_weights=token_weights,
         )
 
-        # integrate the selected and residual tokens
+        # integrate the activated tokens with the residual stream
         outputs = torch.scatter(
             input=inputs,
             dim=1,
@@ -109,9 +103,9 @@ class PraxisMixtureOfDepths(nn.Linear):
         return dict(hidden_states=outputs, aux_loss=aux_loss)
 
     def aux_loss(self, router_logits: torch.Tensor, selected_indices: torch.Tensor):
-        # section 3.5: sampling
         router_targets = torch.zeros_like(router_logits)
         router_targets.scatter_(1, selected_indices, 1.0)
+        # page 7: the aux loss centers the sigmoid of the router’s outputs around 0.5
         return F.binary_cross_entropy_with_logits(
             router_logits.view(-1), router_targets.view(-1)
         )
