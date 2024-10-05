89dac89b4665d4db6f9934686e37bd3adb0743d2
Luciferian Ink <LuciferianInk@protonmail.com>
2024-10-05 13:21:49 -0500

    remove controller code

--- praxis/modules/decoder.py
+++ praxis/modules/decoder.py
@@ -2,7 +2,6 @@ import torch.nn as nn
 from transformers.activations import ACT2FN
 
 from ..configuration_praxis import PraxisConfig
-from .controller import PraxisController
 from .experts import PraxisBlock
 from .router import PraxisMixtureOfDepths
 
@@ -10,35 +9,27 @@ from .router import PraxisMixtureOfDepths
 class PraxisDecoder(nn.Module):
     def __init__(self, config: PraxisConfig):
         super().__init__()
-        self.ctrl = PraxisController(config)
         self.experts = nn.ModuleList()
         self.routers = nn.ModuleList() if config.sparse else None
         for i in range(config.n_layer):
             self.experts.append(PraxisBlock(config))
-            if self.routers is not None:
+            use_router = i % 2 != 0  # if layer is odd
+            if self.routers is not None and use_router:
                 self.routers.append(PraxisMixtureOfDepths(config))
 
     def forward(self, inputs, attention_mask):
         hidden_states = inputs  # Shape: (batch_size, seq_len, n_dim)
         aux_losses = []
 
-        sequence, expert_weights, aux_loss = self.ctrl(hidden_states)
-        aux_losses.append(aux_loss)
-
-        for i, idx in enumerate(sequence):
-
-            expert = self.experts[idx]
-            expert_weight = expert_weights[i]
-            residual = hidden_states
+        for i, expert in enumerate(self.experts):
 
             use_router = i % 2 != 0  # if layer is odd
             if use_router and self.routers is not None:
-                router = self.routers[idx]
-                expert_outputs, aux_loss = router(hidden_states, expert, attention_mask)
+                router = self.routers[(i - 1) // 2]
+                hidden_states, aux_loss = router(hidden_states, expert, attention_mask)
             else:
-                expert_outputs, aux_loss = expert(hidden_states, attention_mask)
+                hidden_states, aux_loss = expert(hidden_states, attention_mask)
 
-            hidden_states = (expert_outputs * expert_weight) + residual
             aux_losses.append(aux_loss)
 
         return hidden_states, sum(aux_losses)
