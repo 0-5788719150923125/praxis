6806dd967ef87e538d46093578e0270e727b1e33
Luciferian Ink <LuciferianInk@protonmail.com>
2024-10-10 17:04:29 -0500

    fixes

--- flow/index.js
+++ flow/index.js
@@ -4,6 +4,6 @@ import App from './App'
 
 import './index.css'
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('app'))
 
 root.render(<App />)
