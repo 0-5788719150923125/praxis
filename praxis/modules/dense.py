b8ea450486c9c77de1ec336c4e22f7524ae1242c
Luciferian Ink <LuciferianInk@protonmail.com>
2024-11-03 07:54:35 -0600

    code quality

---
+++ praxis/modules/dense.py
@@ -0,0 +1,43 @@
+from torch import nn
+from transformers import AutoConfig
+
+from praxis.activations import ACT2FN
+
+
+class PraxisMLP(nn.Sequential):
+    """
+    A standard Multi-Layer Perceptron.
+    """
+
+    __version__ = "0.1.0"
+
+    def __init__(self, config: AutoConfig):
+        super().__init__(
+            OrderedDict(
+                [
+                    ("up", nn.Linear(config.num_dims, 4 * config.num_dims)),
+                    ("act", ACT2FN[config.activation]),
+                    ("dropout", nn.Dropout(config.dropout)),
+                    ("down", nn.Linear(4 * config.num_dims, config.num_dims)),
+                ]
+            )
+        )
+
+
+class PraxisGLU(nn.Module):
+    """
+    A standard MLP, augmented with a Gated Linear Units.
+    """
+
+    __version__ = "0.1.0"
+
+    def __init__(self, config: AutoConfig):
+        super().__init__()
+        self.up = nn.Linear(config.num_dims, 8 * config.num_dims)
+        self.act = ACT2FN[config.activation]
+        self.dropout = nn.Dropout(config.dropout)
+        self.down = nn.Linear(4 * config.num_dims, config.num_dims)
+
+    def forward(self, x):
+        a, b = self.up(x).chunk(2, dim=-1)
+        return self.down(self.dropout(a * self.act(b)))
--- praxis/modules/experts.py
+++ praxis/modules/experts.py
@@ -9,11 +9,19 @@ from transformers import AutoConfig
 
 from praxis.activations import ACT2FN
 from praxis.modules.attention import PraxisAttention
+from praxis.modules.dense import PraxisGLU, PraxisMLP
 from praxis.modules.peer import PraxisPEER
 from praxis.modules.router import PraxisMixtureOfDepths
 from praxis.modules.smear import PraxisSMEAR
 
 input_shape = lambda batch_size, hidden_dim: torch.empty((batch_size, hidden_dim))
+
+EXPERT_REGISTRY = {
+    "mlp": PraxisMLP,
+    "glu": PraxisGLU,
+    "peer": PraxisPEER,
+    "smear": PraxisSMEAR,
+}
 
 
 class PraxisExpert(nn.Module):
@@ -146,50 +154,3 @@ class PraxisBlock(nn.Module):
             return torch.abs(tensor).max().item() < tolerance
         except Exception as e:
             return True
-
-
-class PraxisMLP(nn.Sequential):
-    """
-    A standard Multi-Layer Perceptron.
-    """
-
-    __version__ = "0.1.0"
-
-    def __init__(self, config: AutoConfig):
-        super().__init__(
-            OrderedDict(
-                [
-                    ("up", nn.Linear(config.num_dims, 4 * config.num_dims)),
-                    ("act", ACT2FN[config.activation]),
-                    ("dropout", nn.Dropout(config.dropout)),
-                    ("down", nn.Linear(4 * config.num_dims, config.num_dims)),
-                ]
-            )
-        )
-
-
-class PraxisGLU(nn.Module):
-    """
-    A standard MLP, augmented with a Gated Linear Units.
-    """
-
-    __version__ = "0.1.0"
-
-    def __init__(self, config: AutoConfig):
-        super().__init__()
-        self.up = nn.Linear(config.num_dims, 8 * config.num_dims)
-        self.act = ACT2FN[config.activation]
-        self.dropout = nn.Dropout(config.dropout)
-        self.down = nn.Linear(4 * config.num_dims, config.num_dims)
-
-    def forward(self, x):
-        a, b = self.up(x).chunk(2, dim=-1)
-        return self.down(self.dropout(a * self.act(b)))
-
-
-EXPERT_REGISTRY = {
-    "mlp": PraxisMLP,
-    "glu": PraxisGLU,
-    "peer": PraxisPEER,
-    "smear": PraxisSMEAR,
-}
