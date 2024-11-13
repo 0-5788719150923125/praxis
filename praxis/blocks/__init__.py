75f39aac5d338ca1c4c4d96508bff98ffde413fb
Luciferian Ink <LuciferianInk@protonmail.com>
2024-11-13 00:48:18 -0600

    fix bug

--- praxis/blocks/__init__.py
+++ praxis/blocks/__init__.py
@@ -2,4 +2,4 @@ from praxis.blocks.transformer import PraxisBlock, EXPERT_REGISTRY, EXPERT_CONFI
 from praxis.blocks.nano import PraxisNano
 from praxis.blocks.conv import PraxisConv
 
-BLOCK_REGISTRY = {"default": PraxisBlock, "nano": PraxisNano, "conv": PraxisConv}
+BLOCK_REGISTRY = {"transformer": PraxisBlock, "nano": PraxisNano, "conv": PraxisConv}
