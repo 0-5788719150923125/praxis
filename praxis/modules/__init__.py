2944d36827ec6161f4f68e1a728e4d232ff33dd9
Luciferian Ink <LuciferianInk@protonmail.com>
2024-11-27 00:39:03 -0600

    remove unused code

--- praxis/modules/__init__.py
+++ praxis/modules/__init__.py
@@ -1,4 +1,3 @@
-from praxis.modules.common import MultiIdentity
 from praxis.modules.embeddings import EMBEDDING_REGISTRY
 from praxis.modules.encoding import ENCODING_REGISTRY
 from praxis.modules.experts import EXPERT_CONFIGS, EXPERT_REGISTRY
