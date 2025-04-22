cc91b1d87d15aac765052ca5c06616a28071c89f
Ryan Brooks <LuciferianInk@protonmail.com>
2025-04-22 00:48:01 -0500

    work on lighteval

--- evaluation.py
+++ evaluation.py
@@ -2,12 +2,9 @@ import os
 
 import lighteval
 from lighteval.logging.evaluation_tracker import EvaluationTracker
-
-# from lighteval.models.vllm.vllm_model import VLLMModelConfig
 from lighteval.models.transformers.transformers_model import TransformersModelConfig
 from lighteval.pipeline import ParallelismManager, Pipeline, PipelineParameters
 from lighteval.utils.imports import is_accelerate_available
-from lighteval.utils.utils import EnvConfig
 from transformers import AutoConfig, AutoModel, AutoModelForCausalLM, AutoTokenizer
 
 from praxis import PraxisConfig, PraxisForCausalLM, PraxisModel
@@ -15,15 +12,6 @@ from praxis import PraxisConfig, PraxisForCausalLM, PraxisModel
 AutoConfig.register("praxis", PraxisConfig)
 AutoModel.register(PraxisConfig, PraxisModel)
 AutoModelForCausalLM.register(PraxisConfig, PraxisForCausalLM)
-
-# if is_accelerate_available():
-#     from accelerate import Accelerator, InitProcessGroupKwargs
-
-#     accelerator = Accelerator(
-#         kwargs_handlers=[InitProcessGroupKwargs(timeout=timedelta(seconds=3000))]
-#     )
-# else:
-#     accelerator = None
 
 
 # Global configuration
@@ -36,24 +24,23 @@ def evaluate_model(model=None, max_samples=None):
     evaluation_tracker = EvaluationTracker(
         output_dir=os.path.join(cache_dir, "eval"),
         save_details=True,
-        # push_to_hub=False,
-        # hub_results_org="UNSAFE",
     )
 
     pipeline_params = PipelineParameters(
         launcher_type=ParallelismManager.ACCELERATE,
-        env_config=EnvConfig(cache_dir=os.path.join(cache_dir, "tmp")),
-        override_batch_size=1,
         max_samples=max_samples,
     )
 
     if model is None:
+        # from transformers import GenerationConfig
+
         model_config = TransformersModelConfig(
-            pretrained=os.path.join(cache_dir, "praxis"),
+            model_name=os.path.join(cache_dir, "praxis"),
             device=device,
             tokenizer=f"UNSAFE/praxis-{vocab_size}",
-            accelerator=None,
             model_parallel=False,
+            batch_size=1,
+            # generation_config=GenerationConfig(use_cache=False),
         )
     else:
         model_config = None
@@ -66,7 +53,6 @@ def evaluate_model(model=None, max_samples=None):
         evaluation_tracker=evaluation_tracker,
         model=model,
         model_config=model_config,
-        # custom_task_directory=None,  # if using a custom task
     )
 
     pipeline.evaluate()
@@ -76,56 +62,3 @@ def evaluate_model(model=None, max_samples=None):
 
 if __name__ == "__main__":
     evaluate_model(max_samples=10)
-
-# import os
-
-# import lm_eval
-# from lm_eval.models.huggingface import HFLM
-# from transformers import AutoConfig, AutoModel, AutoModelForCausalLM, AutoTokenizer
-
-# from praxis import PraxisConfig, PraxisForCausalLM, PraxisModel
-
-# AutoConfig.register("praxis", PraxisConfig)
-# AutoModel.register(PraxisConfig, PraxisModel)
-# AutoModelForCausalLM.register(PraxisConfig, PraxisForCausalLM)
-
-# # Global configuration
-# cache_dir = "data"
-# device = "cuda:0"
-# vocab_size = 8192
-
-# # Tokenizer initialization
-# tokenizer = AutoTokenizer.from_pretrained(
-#     f"UNSAFE/praxis-{vocab_size}", cache_dir=cache_dir
-# )
-
-# # https://github.com/EleutherAI/lm-evaluation-harness/blob/main/docs/interface.md
-# # https://github.com/EleutherAI/lm-evaluation-harness/blob/main/docs/model_guide.md
-# eval_model = HFLM(
-#     os.path.join(cache_dir, "praxis"),
-#     backend="causal",
-#     batch_size=1,
-#     tokenizer=tokenizer,
-#     device=device,
-# )
-
-# task_manager = lm_eval.tasks.TaskManager()
-# results = lm_eval.simple_evaluate(
-#     model=eval_model,
-#     tasks=["arc_easy"],
-#     # tasks=[
-#     #     "arc_easy",
-#     #     "arc_challenge",
-#     #     "arithmetic",
-#     #     "glue",
-#     #     "hellaswag",
-#     #     "openbookqa",
-#     #     "piqa",
-#     #     "sciq",
-#     #     "squadv2",
-#     #     "tinyMMLU",
-#     #     "winogrande",
-#     # ],
-#     num_fewshot=0,
-#     task_manager=task_manager,
-# )
