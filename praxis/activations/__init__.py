32ad21804b49177b15c46db4e44e4eed604f3f76
Ryan Brooks <LuciferianInk@protonmail.com>
2025-06-01 14:51:15 -0500

    remove SUGAR

--- praxis/activations/__init__.py
+++ praxis/activations/__init__.py
@@ -9,7 +9,6 @@ from praxis.activations.sin import Sine
 from praxis.activations.sin_cos import SineCosine
 from praxis.activations.sinlu import SinLU
 from praxis.activations.snake import Snake
-from praxis.activations.sugar import SUGAR
 
 ACTIVATION_MAP = dict(
     jagged_sin=JaggedSine,
@@ -21,7 +20,6 @@ ACTIVATION_MAP = dict(
     sin_cos=SineCosine,
     sinlu=SinLU,
     snake=Snake,
-    sugar=SUGAR,
 )
 
 for k, v in ACTIVATION_MAP.items():
